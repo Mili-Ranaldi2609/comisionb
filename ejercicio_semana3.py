a230b97bb6beb1802f64039db24068ca8b37b937
Mili Ranaldi <mranaldi@gmail.com>
2023-08-22 20:14:52 -0300

    Ejercicio semana 3 terminado

---
+++ ejercicio_semana3.py
@@ -0,0 +1,36 @@
+print("Ingrese los datos tal cual han sido pedidos en el ejemplo")
+fecha=input("Ingrese la fecha (dia, DD/MM) ")
+
+dia_semana=fecha[0: fecha.find(',')]
+dia=int(fecha[fecha.find(' ') + 1: fecha.find('/')])
+mes=int(fecha[fecha.find('/')+1:])
+
+dia_semana=dia_semana.lower()
+
+if (dia_semana!="lunes") and (dia_semana!= "martes") and (dia_semana!= "miercoles") and (dia_semana!="jueves")and (dia_semana!="viernes"):
+    print("Se produjo un error reinicie el programa")
+else:
+    if (dia<=31 and  dia>0 ) and (mes<=12 and mes>0):
+        nivel=(input("Ingrese su nivel:(1)Inicial, (2)Intermedio, (3)Avanzado, (4)Practica Hablada, (5)Ingles para viajeros: "))
+        if nivel!="5" and nivel!="4":        
+            y_n=input("Hubo examenes? (si/no): ")
+            y_n=y_n.lower()
+            if y_n=="si":
+                aprobados=int(input("Ingrese la cantidad de aprobados: "))
+                desaprobados=int(input("Ingrese la cantidad de desaprobados: "))
+                porcentaje=(aprobados/(aprobados+desaprobados))*100
+                print(f"El porcentaje de aprobados es de: {porcentaje}%")
+            else:
+                print("Termino el programa")
+        elif nivel=="4":
+            asistencia=int(input("Porcentaje de asistencia: "))
+            if asistencia>50:
+                print("Asistio la mayoria:")
+            else:
+                print("No asistio la mayoria")
+        elif nivel=="5":
+            if dia==1 and (mes==1 or mes==7):
+                print("Comienzo de nuevo ciclo: ")
+                alumnos=int(input("Cantidad de alumnos: "))
+                arancel=float(input("Arancel por alumno: $"))
+                print(f"Ingreso total: ${alumnos*arancel}")
