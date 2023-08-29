18302eea682a08f87374ce0bd1527c615c7e7d9b
Mili Ranaldi <mranaldi@gmail.com>
2023-08-29 16:49:42 -0300

    ejercicio semana 3

---
+++ ejercicio.for.while.py
@@ -0,0 +1,33 @@
+#Ejercicio 1
+
+corrimiento=int(input("Ingrese la cantidad de espacios que se correran las letras: "))
+alfabeto="abcdefghijklmnñopqrstuvwxyz"
+
+for i  in range(5):
+    mensaje=input("Mensaje a encriptar: ").lower()
+    mensaje_encriptado=''
+    for letra in mensaje:
+        if letra in alfabeto:
+            indice=alfabeto.find(letra)
+            indice=(indice+corrimiento)%27
+            mensaje_encriptado+=alfabeto[indice]
+        else: 
+            mensaje_encriptado+=letra 
+    print(f'El mensaje encriptado es: {mensaje_encriptado}')
+
+#Ejercicio 2
+
+num=1
+digito_p=0
+digito_imp=0
+while (num>0 and num!=0):
+    
+    num=int(input("Ingrese un numero: "))
+    if (num%2==0):
+        digito_p=1+digito_p
+    elif num%2!=0:
+        digito_imp=1+digito_imp
+        
+
+print(f"digito pares: {digito_p}") 
+print(f"Digito impares:{digito_imp}")
