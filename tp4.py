17fd5ce87d57931d767f13bb14258b4cf7c85f1d
Mili Ranaldi <mranaldi@gmail.com>
2023-09-05 17:37:32 -0300

    tp4

---
+++ tp4.py
@@ -0,0 +1,17 @@
+#Ejercicio 1 ingles
+X=int(0)
+while X<30:
+    X+=1
+    if X==15:
+        print(f"Se rompio la ejecucio del bucle cuando x valia: {X}")
+        break
+    elif (X==4) or (X==6) or (X==10):
+        print(f"Se salto el valor: {X} ")
+        continue
+    print(f'El valor del bucle es: {X} ')
+
+print(f"La ejecución del bucle se interrumpió cuando x era {X}" )
+
+#Ejercicio 1
+
+
