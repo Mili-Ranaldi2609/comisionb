4d7152e1864d694b2120578484fbc4c557d09904
Mili Ranaldi <mranaldi@gmail.com>
2023-09-19 18:22:42 -0300

    ...

--- ejercicio.practica.funciones.py
+++ ejercicio.practica.funciones.py
@@ -1,6 +1,15 @@
+#Funcion
+def add_digits(number):
+    add=0
+    while number!=0:
+        digit=number%10
+        add+=digit
+        number//=10
+    return add
+
+#Programa principal
 number1=1
 while number1!=0:
-    print("Ingrese el numero a procesar: ")
-
-    def add_digits(number):
-        return
+    number1=int(input("Ingrese el numero a procesar: "))
+    print(f"Suma: {add_digits(number1)}")
+    
