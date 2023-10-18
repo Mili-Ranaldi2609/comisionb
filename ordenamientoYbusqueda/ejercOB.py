1a6288606aae8a6202802bc6c9fd43d758e45819
Mili Ranaldi <mranaldi@gmail.com>
2023-10-18 17:48:23 -0300

    terminado

--- ordenamientoYbusqueda/ejercOB.py
+++ ordenamientoYbusqueda/ejercOB.py
@@ -1,5 +1,7 @@
-from obFunc import*
+from obFunc import *
+
 # Ejemplo de Bubble sort
+print("Ordenamiento bubble sort")
 array = [64, 34, 25, 12, 22, 11, 90]
 bubble_sort(array)
 print("Array ordenado:")
@@ -7,67 +9,41 @@ for i in range(len(array)):
     print(array[i])
 
 #Ejemplo de Selection sort
-def selection_sort(arr):
-    for i in range(len(arr)):
-        min_idx = i
-        for j in range(i + 1, len(arr)):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-
-"""principal"""
+print("Ordenamiento selection sort")
 listS = [9, 5, 2, 7, 1]
 selection_sort(listS)
 print(listS)
 
 #Ejemplo de Insert sort
-def insert_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-
-"""principal"""
+print("Ordenamiento insert sort")
 list = [9, 5, 2, 7, 1]
 insert_sort(list)
 print(list)
 
 #Ejemplo de Merge sort
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-
-        merge_sort(left_half)
-        merge_sort(right_half)
-
-        i = j = k = 0
-
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
-
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
-
-"""principal"""
+print("Ordenamiento merge sort")
 listM = [9, 5, 2, 7, 1]
 merge_sort(listM)
 print(listM)
 
+#Ejemplo busqueda lineal
+print("Busqueda lineal")
+my_list = [1, 3, 5, 7, 9, 11, 13]
+element_search = 7
+r = linear_search(my_list, element_search)
+
+if r != -1:
+    print(f"Elemento {element_search} encontrado en el índice {r}")
+else:
+    print(f"Elemento {element_search} no encontrado en la lista")
+
+#Ejemplo de busqueda binaria
+print("Busqueda binaria")
+listB = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+element_s = 11
+res = binary_search(listB, element_s)
+
+if res != -1:
+    print(f"El elemento {element_s} se encuentra en la posición {res}.")
+else:
+    print(f"El elemento {element_s} no se encuentra en la lista.")
